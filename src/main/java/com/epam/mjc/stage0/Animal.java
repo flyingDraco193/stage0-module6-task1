7925af5afb578416f1ad8cc4a65dd99a66cf3171
Gempton <gemptonilya@gmail.com>
2024-03-23 11:51:38 +0300

    Done

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,44 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String description = null;
+        if (hasFur) {
+            if (numberOfPaws == 1) {
+                description = "This animal is mostly " + color + ". It has " + numberOfPaws + " paw and a fur.";
+            } else if (numberOfPaws > 1) {
+                description = "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and a fur.";
+            }
+        } else {
+            if (numberOfPaws == 1) {
+                description = "This animal is mostly " + color + ". It has " + numberOfPaws + " paw and no fur.";
+            } else if (numberOfPaws > 1) {
+                description = "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and no fur.";
+            }
+        }
+
+        return description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,29 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+    public Bird() {
+        super("blue", 2, false);
+
+    }
+
+    @Override
+    public String getDescription() {
+        String description = null;
+        if (super.isHasFur()) {
+            if (super.getNumberOfPaws() == 1) {
+                description = "This animal is mostly " + super.getColor() + ". It has " + super.getNumberOfPaws() + " paw and a fur. Moreover, it has 2 wings and can fly.";
+            } else if (super.getNumberOfPaws() > 1) {
+                description = "This animal is mostly " + super.getColor() + ". It has " + super.getNumberOfPaws() + " paws and a fur. Moreover, it has 2 wings and can fly.";
+            }
+        } else {
+            if (super.getNumberOfPaws() == 1) {
+                description = "This animal is mostly " + super.getColor() + ". It has " + super.getNumberOfPaws() + " paw and no fur. Moreover, it has 2 wings and can fly.";
+            } else if (super.getNumberOfPaws() > 1) {
+                description = "This animal is mostly " + super.getColor() + ". It has " + super.getNumberOfPaws() + " paws and no fur. Moreover, it has 2 wings and can fly.";
+            }
+        }
+
+        return description;
+    }
 }
